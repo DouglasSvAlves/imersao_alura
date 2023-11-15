2433f2e0fe5d3fff41ca13a044d09d2b4fd9025f
Douglas Silva Alves <149844295+DouglasSvAlves@users.noreply.github.com>
2023-11-14 23:17:34 -0300

    Update script.js

--- Aula02/script.js
+++ Aula02/script.js
@@ -21,11 +21,11 @@ while (valorChute != numeroSecreto) {
 // Desafio Número 2
 
 let numeroSecreto = parseInt(Math.random() * 1000 + 1);  //Lembrete!!!! NÂO USE CONST. USE O VAR OU LET
-let valorChute = -1
-let chutes = 0
+let valorChute = -1;
+let chutes = 0;
 
-while (valorChute != numeroSecreto) {
-    valorChute = prompt('Digite um número entre 1 e 1000')
+while (valorChute != numeroSecreto && chutes < 5) {
+    valorChute = prompt('Digite um número entre 0 e 1000')
     if (valorChute == numeroSecreto) {
         alert(`Você Acertou na tentativa número ${chutes}!`)
     } else if (valorChute > numeroSecreto) {
@@ -37,3 +37,35 @@ while (valorChute != numeroSecreto) {
     }
 }
 
+if (chutes >= 5) {
+    alert(`Você excedeu o número de tentativas. O valor secreto era ${numeroSecreto}`)
+}
+
+
+/*
+var numeroSecreto = parseInt(Math.random() * 1000 + 1); // Use var ou let, conforme sua preferência
+var valorChute = -1;
+var chutes = 0;
+
+while (valorChute != numeroSecreto && chutes < 6) {
+    valorChute = parseInt(prompt('Digite um número entre 1 e 1000'));
+
+    if (!isNaN(valorChute)) {
+        if (valorChute == numeroSecreto) {
+            alert(`Você Acertou na tentativa número ${chutes + 1}!`);
+        } else if (valorChute > numeroSecreto) {
+            chutes++;
+            alert(`Errou... o número secreto é menor do que ${valorChute}. Você já tentou ${chutes} vezes.`);
+        } else {
+            chutes++;
+            alert(`Errou... o número secreto é maior do que ${valorChute}. Você já tentou ${chutes} vezes.`);
+        }
+    } else {
+        alert('Por favor, digite um número válido.');
+    }
+}
+
+if (chutes >= 6) {
+    alert('Você excedeu o número de tentativas. O número secreto era: ' + numeroSecreto);
+}    
+*/
